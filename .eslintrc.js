ea2ca1a99a603e71c72326c8f25b2bd871d79b71
bulandent <1326895569@qq.com>
2021-07-09 18:10:42 +0800

    feat: pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
-	extends: [
-		'alloy',
-		'alloy/typescript',
-	],
+	extends: ['alloy', 'alloy/typescript'],
 	rules: {
+		'@typescript-eslint/no-inferrable-types': 'off'
 	}
 }
---
+++ src/01-primitive-data-types/boolean.ts
@@ -0,0 +1,5 @@
+const isDone: boolean = false
+let myName = 'aaa'
+
+// 编译报错
+const hasContain: Boolean = true
